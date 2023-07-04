2c42ce9ac1e0c4a9f77587bc0b1208b8b05243d8
sehatakuro <sehatakuro@gmail.com>
2023-07-04 23:09:56 +0900

    ドッカー関係

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM debian:bullseye-slim
+
+RUN apt update -y && apt install -y python3 python3-pip
+WORKDIR /app
+COPY requirements.txt /app/requirements.txt
+RUN pip3 install -r requirements.txt
+COPY . /app
+CMD ["python3","scheduler.py"]
--- komachan.py
+++ komachan.py
@@ -2,26 +2,24 @@ from mastodon import Mastodon
 import img_choice
 from os import getenv
 
-CLIENT_KEY = getenv("KOMACHAN_CLIENT_KEY")
-CLIENT_SECRET = getenv("KOMACHAN_CLIENT_SECRET")
-ACCESS_TOKEN = getenv("KOMACHAN_ACCESS_TOKEN")
 
-# 初期化
-api = Mastodon(
-    api_base_url = "https://mstdn.giratina.net",
-    client_id = CLIENT_KEY,
-    client_secret = CLIENT_SECRET,
-    access_token = ACCESS_TOKEN
-)
-
-# 画像投稿
-img_random = img_choice.post_img("komachan_img/*")
-
-img1 = api.media_post(img_random)
-media_files = [img1]
-
-api.status_post(
-    status = "",
-    visibility="unlisted",
-    media_ids = media_files
+def run():
+    # 初期化
+    api = Mastodon(
+        api_base_url = "https://mstdn.giratina.net",
+        client_id = getenv("KOMACHAN_CLIENT_KEY"),
+        client_secret = getenv("KOMACHAN_CLIENT_SECRET"),
+        access_token = getenv("KOMACHAN_ACCESS_TOKEN")
     )
+
+    # 画像投稿
+    img_random = img_choice.post_img("komachan_img/*")
+
+    img1 = api.media_post(img_random)
+    media_files = [img1]
+
+    api.status_post(
+        status = "",
+        visibility="unlisted",
+        media_ids = media_files
+        )
